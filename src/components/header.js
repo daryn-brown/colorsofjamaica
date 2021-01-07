beaa7d56f68f310f453d53d1d1a33330cee607cb
Daryn <darynnbrown@gmail.com>
2021-01-07 14:33:02 -0500

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div className='logo'>
+                <span>COLORS OF JAMAICA</span>
+            </div>
+            <div className='creator'>
+                <span>BY DARYN BROWN</span>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
